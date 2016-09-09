0a0f30eaec26a1d454d143130185dec28f812fa2
Jessie <jessiereffet@hotmail.com>
2016-09-09 22:30:25 +0200

    Improve gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,11 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
-import jade from 'gulp-jade';
 import babelify from 'babelify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
 import browserify from 'browserify';
 import exorcist from 'exorcist';
+import eslint from 'gulp-eslint';
 
 gulp.task('sass', function() {
   return gulp.src("./src/scss/*.scss")
@@ -13,22 +13,26 @@ gulp.task('sass', function() {
     .pipe(gulp.dest("./public/css"));
 });
 
-gulp.task('jade', function() {
-   gulp.src(['./src/templates/*.jade'])
-   .pipe(jade({pretty: true, doctype: 'html'}))
-   .pipe(gulp.dest('./public/views'));
-});
-
 gulp.task('babel', function() {
   return browserify({entries: './src/app.js', extensions: ['.js'], debug: true})
     .transform(babelify.configure({sourceMapRelative: 'src'}))
     .bundle()
-    .on('error', console.error.bind(console))
+    .on('error', function(error){
+      console.error( '\nError: ', error.message, '\n');
+      this.emit('end');
+    })
     .pipe(exorcist('./public/bundle.js.map'))
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(gulp.dest('./public'));
   })
 
-gulp.watch("./src/scss/*.scss", ['sass']);
-gulp.watch("./src/**/*.jade", ['jade']);
+gulp.task('lint', function() {
+  return gulp.src(['./src/**/*.js', 'gulpfile.babel.js']).pipe(eslint())
+  .pipe(eslint.format());
+});
+
+gulp.task('watch', () => {
+  gulp.watch("./src/scss/*.scss", ['sass']);
+  gulp.watch("./src/**/*.js", ['babel', 'lint']);
+});
