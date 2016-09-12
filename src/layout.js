5600bf14b82a784917aa83ebbdd19b0b25989e67
Jessie <jessiereffet@hotmail.com>
2016-09-12 17:43:12 +0200

    Style navigation bar

--- src/layout.js
+++ src/layout.js
@@ -5,20 +5,22 @@ class Layout extends React.Component{
   render() {
     return (
       <div>
-        <ul>
-          <li>
-            <Link to="/color-table">Color Table</Link>
-          </li>
-          <li>
-            <Link to="/notebook">Shortcuts</Link>
-          </li>
-          <li>
-            <Link to="/playground">Playground</Link>
-          </li>
-        </ul>
-        <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/playground">Playground</Link>
+            </li>
+            <li>
+              <Link to="/color-table">Color Table</Link>
+            </li>
+            <li>
+              <Link to="/notebook">Shortcuts</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
           {this.props.children}
-        </div>
+        </main>
       </div>
     );
   }
